7ce28fd167e6bfade270c87af1d6ba719ade9a3b
alef <alef@whoisdrwho.net>
2016-02-22 23:23:33 +0300

    add keyword and cljs-reader

--- src/datascript_mori/core.cljs
+++ src/datascript_mori/core.cljs
@@ -3,3 +3,9 @@
     [datascript.core :as d]
     [datascript.js :as djs]
     [mori :as m]))
+
+(defn ^:export reader [query]
+  (cljs.reader/read-string query))
+
+(defn ^:export str_to_keyword [s]
+  (keyword s))
