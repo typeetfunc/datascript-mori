3bfbfc6488aaf6e5fd1903abe3e22809ed7d5f93
alef <alef@whoisdrwho.net>
2016-02-27 15:11:31 +0300

    add helpers

--- src/datascript_mori/core.cljs
+++ src/datascript_mori/core.cljs
@@ -5,6 +5,11 @@
     [mori :as m]
     [mori.extra :as me]))
 
-(def ^:export reader cljs.reader/read-string)
-
-(def ^:export str_to_keyword keyword)
+(def ^:export schema_to_clj      djs/schema->clj)
+(def ^:export entity_to_clj      djs/entity->clj)
+(def ^:export entities_to_clj    djs/entities->clj)
+(def ^:export tempids_to_js      djs/tempids->js)
+(def ^:export tx_report_to_js    djs/tx-report->js)
+(def ^:export js_to_Datom        djs/js->Datom)
+(def ^:export pull_result_to_js  djs/pull-result->js)
+(def ^:export keywordize  djs/keywordize)
